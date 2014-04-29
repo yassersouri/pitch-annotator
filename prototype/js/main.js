4f19b0af968c1f86c452c08da1d19f160d7d4bd7
yassersouri <yassersouri@gmail.com>
2014-04-30 01:33:13 +0430

    a final version of the prototype that is actually usefull

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -1,3 +1,19 @@
+var Line = Backbone.Model.extend({
+    defaults: {
+        "x1": 0,
+        "y1": 0,
+        "x2": 0,
+        "y2": 0,
+        "hover": false
+    }
+});
+
+var Lines = Backbone.Collection.extend({
+    model: Line
+});
+
+var lines = new Lines();
+
 $(function(){
     var img = $('#theimage');
 
@@ -19,30 +35,100 @@ $(function(){
     canvas.width = drawCanvas.width = width;
     canvas.height = drawCanvas.height = height;
 
-    var Line = Backbone.Model.extend({
-        
-    });
-
     drawCanvas.onmousedown = function(e) {
         if(started){
             started = false;
-            lastX = e.pageX - this.offsetLeft;
-            lastY = e.pageY - this.offsetTop;
+            lastX = e.pageX - this.offsetLeft - 10;
+            lastY = e.pageY - this.offsetTop - 10;
             
             clearCxt(drawCtx);
-
-            drawLine(ctx, startX, startY, lastX, lastY);
+            lines.add(new Line({x1: startX, y1: startY, x2: lastX, y2:lastY}));
         } else {
             started = true;
-            startX = e.pageX - this.offsetLeft;
-            startY = e.pageY - this.offsetTop;
+            startX = e.pageX - this.offsetLeft - 10;
+            startY = e.pageY - this.offsetTop - 10;
 
             drawCircle(drawCtx, startX, startY);
         }
     };
 
-    function drawLine (ctx, x1, y1, x2, y2) {
-        ctx.strokeStyle = "#000";
+    lines.on('add', function(){
+        drawAllLines(ctx, lines);
+    });
+
+    lines.on('change', function(){
+        drawAllLines(ctx, lines);
+    });
+
+    lines.on('remove', function(){
+        drawAllLines(ctx, lines);
+    });
+
+    var Edit = Backbone.View.extend({
+        tagName: 'div',
+
+        template: _.template($('#edit-template').html()),
+
+        events: {
+            "change input": "update",
+            "click a.delete": "delete",
+            "mouseover a.delete": "hovered",
+            "mouseleave a.delete": "unhovered"
+        },
+
+        initialize: function() {
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        update: function(e) {
+            newX1 = this.$('input.x1').val();
+            newY1 = this.$('input.y1').val();
+            newX2 = this.$('input.x2').val();
+            newY2 = this.$('input.y2').val();
+            this.model.set({'x1': newX1, 'y1': newY1, 'x2': newX2, 'y2': newY2});
+        },
+
+        delete: function() {
+            this.model.destroy();
+        },
+
+        hovered: function() {
+            this.model.set({'hover': true})
+        },
+
+        unhovered: function() {
+            this.model.set({'hover': false})
+        }
+    });
+
+    var Edits = Backbone.View.extend({
+        el: $('#edits'),
+
+        initialize: function() {
+            this.listenTo(lines, 'add', this.addOne);
+        },
+
+        addOne: function(line) {
+            var edit = new Edit({model: line});
+            this.$el.append(edit.render().el);
+        }
+
+    });
+
+    var App = new Edits;
+
+    function drawLine (ctx, x1, y1, x2, y2, hover) {
+        ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+        if (hover) {
+            ctx.strokeStyle = 'rgba(255, 255, 0, 1)';
+        }else{
+            ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+        };
         ctx.beginPath();
         ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y2);
@@ -56,7 +142,16 @@ $(function(){
         ctx.fill();
     }
 
-    function clearCxt (ctx) {
+    function clearCxt(ctx) {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     }
+
+    function drawAllLines(ctx, lines){
+        clearCxt(ctx);
+        lines.each(function(line){
+            drawLine(ctx, line.get('x1'), line.get('y1'), line.get('x2'), line.get('y2'), line.get('hover'));
+        });
+    }
 });
+
+
