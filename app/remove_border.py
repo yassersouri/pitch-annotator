bd41840e36010f8d90b438556b3c93d73482aa09
yassersouri <yassersouri@gmail.com>
2014-05-01 23:18:49 +0430

    add database constant

---
+++ app/remove_border.py
@@ -0,0 +1,17 @@
+import cv2
+import glob
+
+FOLDER = '/Users/yasser/Desktop/azadi/frames/'
+EXT = 'jpg'
+
+def main():
+    files = glob.glob("%s*.%s" % (FOLDER, EXT))
+
+    for file in files:
+        img = cv2.imread(file)
+        img2 = img[:-2, 10:-15]
+        cv2.imwrite(file, img2)
+        print file
+
+if __name__ == '__main__':
+    main()
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
 
+DATA_BASE_FOLDER = '/Users/yasser/sci-repo/pitchdataset'
+
 @app.route('/')
 @app.route('/index')
 def index():
