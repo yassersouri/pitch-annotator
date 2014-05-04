98d179c619e9f1cb7399e50a313f8705c56c8df4
yassersouri <yassersouri@gmail.com>
2014-05-04 18:05:50 +0430

    add the first dynamic version of the app
    also add bootstrap

---
+++ app/static/js/main.js
@@ -0,0 +1,158 @@
+var Line = Backbone.Model.extend({
+    defaults: {
+        "x1": 0,
+        "y1": 0,
+        "x2": 0,
+        "y2": 0,
+        "hover": false
+    }
+});
+
+var Lines = Backbone.Collection.extend({
+    model: Line
+});
+
+var lines = new Lines();
+
+$(function(){
+    var img = $('#theimage');
+
+    var canvas = document.getElementById("canvas"),
+        ctx = canvas.getContext("2d"),
+        drawCanvas = document.getElementById("drawCanvas"),
+        drawCtx = drawCanvas.getContext("2d"),
+        started = false,
+        lastX = 0,
+        lastY = 0,
+        curX = 0,
+        curY = 0,
+        startX = 0,
+        startY = 0,
+        lineThickness = 1
+        width = img.width(),
+        height = img.height();
+
+    canvas.width = drawCanvas.width = width;
+    canvas.height = drawCanvas.height = height;
+
+    drawCanvas.onmousedown = function(e) {
+        if(started){
+            started = false;
+            lastX = e.pageX - this.offsetLeft - 10;
+            lastY = e.pageY - this.offsetTop - 10;
+            
+            clearCxt(drawCtx);
+            lines.add(new Line({x1: startX, y1: startY, x2: lastX, y2:lastY}));
+        } else {
+            started = true;
+            startX = e.pageX - this.offsetLeft - 10;
+            startY = e.pageY - this.offsetTop - 10;
+
+            drawCircle(drawCtx, startX, startY);
+        }
+    };
+
+    lines.on('add', function(){
+        drawAllLines(ctx, lines);
+    });
+
+    lines.on('change', function(){
+        drawAllLines(ctx, lines);
+    });
+
+    lines.on('remove', function(){
+        drawAllLines(ctx, lines);
+    });
+
+    var Edit = Backbone.View.extend({
+        tagName: 'div',
+
+        template: _.template($('#edit-template').html()),
+
+        events: {
+            "change input": "update",
+            "click a.delete": "delete",
+            "mouseover .view": "hovered",
+            "mouseleave .view": "unhovered"
+        },
+
+        initialize: function() {
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        update: function(e) {
+            newX1 = this.$('input.x1').val();
+            newY1 = this.$('input.y1').val();
+            newX2 = this.$('input.x2').val();
+            newY2 = this.$('input.y2').val();
+            this.model.set({'x1': newX1, 'y1': newY1, 'x2': newX2, 'y2': newY2});
+        },
+
+        delete: function() {
+            this.model.destroy();
+            return false;
+        },
+
+        hovered: function() {
+            this.model.set({'hover': true})
+        },
+
+        unhovered: function() {
+            this.model.set({'hover': false})
+        }
+    });
+
+    var Edits = Backbone.View.extend({
+        el: $('#edits'),
+
+        initialize: function() {
+            this.listenTo(lines, 'add', this.addOne);
+        },
+
+        addOne: function(line) {
+            var edit = new Edit({model: line});
+            this.$el.append(edit.render().el);
+        }
+
+    });
+
+    var App = new Edits;
+
+    function drawLine (ctx, x1, y1, x2, y2, hover) {
+        ctx.strokeWidth = 3;
+        if (hover) {
+            ctx.strokeStyle = '#FFDC00';
+        }else{
+            ctx.strokeStyle = '#001F3F';
+        };
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+    }
+
+    function drawCircle (ctx, x, y) {
+        ctx.fillStyle = '#0074D9';
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, 2 * Math.PI, true);
+        ctx.fill();
+    }
+
+    function clearCxt(ctx) {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+
+    function drawAllLines(ctx, lines){
+        clearCxt(ctx);
+        lines.each(function(line){
+            drawLine(ctx, line.get('x1'), line.get('y1'), line.get('x2'), line.get('y2'), line.get('hover'));
+        });
+    }
+});
+
+
--- app/views.py
+++ app/views.py
@@ -1,8 +1,39 @@
+from flask import render_template
+import os
 from app import app
 
 DATA_BASE_FOLDER = '/Users/yasser/sci-repo/pitchdataset'
 
+IMAGE_FOLDER = 'images'
+GROUND_TRUTH_FOLDER = 'groundTruth'
+
+
+def find_sub_dirs(directory):
+    subdirs = []
+    for x in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, x)):
+            subdirs.append(x)
+    return subdirs
+
+def find_all_images(directory):
+    files = []
+    for x in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, x)):
+            files.append({'filename': x, 'basename': os.path.splitext(x)[0]})
+    return files
+
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, world!"
+    images_folder = os.path.join(DATA_BASE_FOLDER, IMAGE_FOLDER)
+    subdirs = find_sub_dirs(images_folder)
+
+    images = {}
+    for subd in subdirs:
+        images[subd] = find_all_images(os.path.join(images_folder, subd))
+
+    data = {
+        'subdirs': subdirs,
+        'images': images
+    }
+    return render_template("main.html", **data)
