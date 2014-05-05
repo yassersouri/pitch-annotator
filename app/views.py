f2f859d4341e6119e960a9dd5712544f88c986e6
yassersouri <yassersouri@gmail.com>
2014-05-05 17:28:33 +0430

    make the prototype dynamic

--- app/views.py
+++ app/views.py
@@ -6,7 +6,10 @@ DATA_BASE_FOLDER = '/Users/yasser/sci-repo/pitchdataset'
 
 IMAGE_FOLDER = 'images'
 GROUND_TRUTH_FOLDER = 'groundTruth'
+GT_EXT = '.json'
 
+images_folder = os.path.join(DATA_BASE_FOLDER, IMAGE_FOLDER)
+ground_truth_folder = os.path.join(DATA_BASE_FOLDER, GROUND_TRUTH_FOLDER)
 
 def find_sub_dirs(directory):
     subdirs = []
@@ -15,25 +18,41 @@ def find_sub_dirs(directory):
             subdirs.append(x)
     return subdirs
 
-def find_all_images(directory):
+def find_all_images(directory, subd):
     files = []
     for x in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, x)):
-            files.append({'filename': x, 'basename': os.path.splitext(x)[0]})
+            x_basename = os.path.splitext(x)[0]
+            ground_truth_file_name = os.path.join(os.path.join(ground_truth_folder, subd), x_basename + GT_EXT)
+            has_ground_truth = os.path.isfile(ground_truth_file_name)
+            files.append({'filename': x, 'basename': x_basename, 'truth': has_ground_truth})
+    # sort the files so that the files with no ground truth are up in the list
+    files.sort(key=lambda x: x['truth'], reverse=False)
     return files
 
 @app.route('/')
 @app.route('/index')
 def index():
-    images_folder = os.path.join(DATA_BASE_FOLDER, IMAGE_FOLDER)
     subdirs = find_sub_dirs(images_folder)
 
     images = {}
     for subd in subdirs:
-        images[subd] = find_all_images(os.path.join(images_folder, subd))
+        images[subd] = find_all_images(os.path.join(images_folder, subd), subd)
 
     data = {
         'subdirs': subdirs,
         'images': images
     }
     return render_template("main.html", **data)
+
+
+@app.route('/a/<subd>/<img_name>')
+def annotate(subd, img_name):
+    data = {
+        'image_src': "/static/dataset/images/%s/%s" % (subd, img_name),
+        'subd' : subd,
+        'img_name': img_name
+    }
+
+    return render_template("annotate.html", **data)
+    
