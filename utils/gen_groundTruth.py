b6749bf14f45eabaa9a9af7013969c68dd2c96cf
yassersouri <yassersouri@gmail.com>
2014-05-11 18:47:13 +0430

    draw lines on the image

--- utils/gen_groundTruth.py
+++ utils/gen_groundTruth.py
@@ -1,13 +1,33 @@
 import scipy.io
+import glob
+import json
+import cv2
+import numpy
 
-FOLDER = '/Users/yasser/sci-repo/pitchdataset/groundTruth/%s' % 'train'
+json_file_name = '/Users/yasser/sci-repo/pitchdataset/groundTruth/train/2.json'
+img_file_name = '/Users/yasser/sci-repo/pitchdataset/images/train/2.jpg'
 
-test_mat = '/Users/yasser/sci-repo/BSR_dataset/16068.mat'
-test_image = '/Users/yasser/sci-repo/BSR_dataset/16068.jpg'
+def do_stuff(json_file_name, img_file_name):
+    img = cv2.imread(img_file_name)
+    shape = img.shape[0:2]
+    lines = 0
+    with open(json_file_name, 'r') as f:
+        lines = json.loads(f.read())
+    
+    edge = numpy.zeros(shape, dtype=numpy.uint8)
+    segs_pre = numpy.ones(shape, dtype=numpy.uint8) * 255
+    segs = numpy.zeros(shape, dtype=numpy.uint8)
+
+    for line in lines:
+        cv2.line(segs_pre, (int(line['x1']), int(line['y1'])), (int(line['x2']), int(line['y2'])), (0, 0, 0), thickness=1)
+        cv2.line(edge, (int(line['x1']), int(line['y1'])), (int(line['x2']), int(line['y2'])), (255, 255, 255), thickness=1)
+
+    cv2.imshow('test', segs_pre)
+    cv2.waitKey(0)
 
 def main():
-    loaded_mat = scipy.io.loadmat(test_mat)
-    print loaded_mat.keys()
+
+    do_stuff(json_file_name, img_file_name)
 
 if __name__ == '__main__':
     main()
